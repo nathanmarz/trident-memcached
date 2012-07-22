0811d9751a4ed8da3c19f4c476e873d01dba73e8
Nathan Marz <nathan@twitter.com>
2012-07-22 14:26:18 -0700

    fix artifact name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject trident-memcached "0.0.1-SNAPSHOT"
+(defproject storm/trident-memcached "0.0.1-SNAPSHOT"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
