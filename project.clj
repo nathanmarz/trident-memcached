23741eb31d2b05bf94de1e053b3d997affc96f85
Nathan Marz <nathan@twitter.com>
2012-09-04 15:25:25 -0700

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject storm/trident-memcached "0.0.2-wip3"
+(defproject storm/trident-memcached "0.0.2-wip4"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
