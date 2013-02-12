f43bbe5b04e5a4b314e21aa3ee30c46cf1d6a7aa
Nathan Marz <nathan@twitter.com>
2013-02-12 15:04:06 -0800

    0.0.5-wip2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject storm/trident-memcached "0.0.5-wip1"
+(defproject storm/trident-memcached "0.0.5-wip2"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
